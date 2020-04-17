83897316b620c810a3932c3c3b0ae344f8a2f80c
Vic <Vic@DESKTOP-GO5GRHQ>
2020-04-17 18:19:06 -0400

    Commit: Fixed some issues with the code

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.Random;
+
 /**
  * A class to be used as the base Card class for the project. Must be general
  * enough to be instantiated for any Card game. Students wishing to add to the code 
@@ -20,20 +22,27 @@ public class Card
      * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
      */
     public enum Suit {HEARTS, CLUBS,SPADES,DIAMONDS};
-        public enum Value{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
+    public enum Value{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
         private final Suit suit;
         private final Value value;
-
-        public Card(Suit s, Value gVal)
+        
+        public Card(int s, int gVal)
+        {
+           suit =Suit.values()[s];
+           value= Value.values()[gVal];
+        }
+        public Card(){
+           suit =Suit.values()[0];
+           value= Value.values()[0];
+        }
+        /*public Card(Suit s, Value gVal)
         {
            suit =s;
            value= gVal;
-        }
+        }*/
     public Value getValue() {
         return this.value;
     }
-
-
     public Suit getSuit() {
         return this.suit;
         }
