04df14ed83ea213a671a40c8e749ed15c5aa0195
991320306 <ahmadalsayyed93@gmail.com>
2020-04-17 22:15:32 -0400

    dealHand() logic working/Card hand logic, Player logic partially completed

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -1,16 +1,19 @@
 
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+
 
 public class Player 
 {
     private String playerID; //the unique ID for this player
-
-   
+    private int booksCollected;
+    private ArrayList<Card> cards;
     public Player(String name)
     {
         playerID= name;
     }
+    
 
   
     public String getPlayerID() 
@@ -23,8 +26,38 @@ public class Player
     {
         playerID = givenID;
     }
-
+    
+    
+    
+    public ArrayList<Card> startTurn(ArrayList<Card> cards,Player player){
+        ArrayList<Card> otherPlayerHand = new ArrayList<Card>();
+        this.cards = cards;
+       
+        otherPlayerHand = player.cards;
+       int randS = 0 + (int)(Math.random()*((3 - 0) + 1 ));
+       int randV = 0 + (int)(Math.random()*((12 - 0) + 1 ));
+       int randV2 = 0 + (int)(Math.random()*((12 - 0) + 1 ));
+       int randS2 = 0 + (int)(Math.random()*((3 - 0) + 1 ));
+       
+       if(cards.get(randV) == otherPlayerHand.get(randV2) && cards.get(randS) == otherPlayerHand.get(randS2) ){
+           System.out.println("You earned one book!!");
+           booksCollected+= 1;
+           
+       }
+       else if(!(cards.get(randV) == otherPlayerHand.get(randV2) && cards.get(randS) == otherPlayerHand.get(randS2))){
+           System.out.println("Go fish!");
+           goFish();
+       }
+       
+        return cards;
+    }
   
+    private void goFish(){
+         int randS = 0 + (int)(Math.random()*((3 - 0) + 1 ));
+         int randV = 0 + (int)(Math.random()*((12 - 0) + 1 ));
+        cards.add(new Card(randS,randV));
+    }
+   
     public void play(){}
 
 }
