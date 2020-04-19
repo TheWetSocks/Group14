c1ba0bae28e735fee1e50e0bc30a1566a1d25c3f
Vic <Vic@DESKTOP-GO5GRHQ>
2020-04-19 19:45:26 -0400

    Commit: Comments added to the card class

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -8,47 +8,78 @@ package ca.sheridancollege.project;
 import java.util.Random;
 
 /**
- * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code 
- * should remember to add themselves as a modifier.
- * @author dancye
+ * The Card class holds the enum of the cards available (all 52). It holds the
+ * methods to get the suit and value as well as print the card.
+ *
+ * @modifier Ahmad Al Sayyed, Daniel Ayala, Max Ivashkevich
+ * @version 1.7
+ * @since 1.0 Date Complete: 2020-04-19
  */
-public class Card 
-{
+public class Card {
+
     //default modifier for child classes
-    
+    public enum Suit {
+        HEARTS, CLUBS, SPADES, DIAMONDS
+    };
+
+    public enum Value {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    };
+    private final Suit suit;
+    private final Value value;
+
     /**
-     * Students should implement this method for their specific children classes 
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     * This represents the suit and value the card holds
+     *
+     * @param s the cars suit
+     * @param gVal the cards value
      */
-    public enum Suit {HEARTS, CLUBS,SPADES,DIAMONDS};
-    public enum Value{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
-        private final Suit suit;
-        private final Value value;
-        
-        public Card(int s, int gVal)
-        {
-           suit =Suit.values()[s];
-           value= Value.values()[gVal];
-        }
-        public Card(){
-           suit =Suit.values()[0];
-           value= Value.values()[0];
-        }
-        /*public Card(Suit s, Value gVal)
-        {
-           suit =s;
-           value= gVal;
-        }*/
+    public Card(int s, int gVal) {
+        suit = Suit.values()[s];
+        value = Value.values()[gVal];
+    }
+
+    /**
+     * @return the card with the values from the enum
+     */
+
+    public Card() {
+        suit = Suit.values()[0];
+        value = Value.values()[0];
+    }
+
+    /**
+     * A blank constructor for card class
+     *
+     * @return a blank card
+     */
     public Value getValue() {
         return this.value;
     }
+
+    /**
+     * gets the cards value
+     *
+     * @return the value of the card at a specific location
+     */
+
     public Suit getSuit() {
         return this.suit;
-        }
-    
+    }
+
+    /**
+     * gets the cards suit
+     *
+     * @return the suit of the card at a specific location
+     */
+
     @Override
-    public String toString(){return "";}
-    
-    // this is a test
+    public String toString() {
+        return "";
+    }
+    /**
+     * A test to see if the player has cards in their hand
+     * @return a blank card inside an array
+     */
+
 }
