352068bec87858b26a5555cfc3f0f8f3f9d8c511
Vic <Vic@DESKTOP-GO5GRHQ>
2020-04-19 19:53:37 -0400

    Commit: Comments added to the DealCards class

--- src/ca/sheridancollege/project/DealCards.java
+++ src/ca/sheridancollege/project/DealCards.java
@@ -4,34 +4,46 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.project;
+
 import java.util.Random;
 import java.util.ArrayList;
 
 /**
  *
- * @author Redbark
+ * @modifier Ahmad Al Sayyed, Daniel Ayala, Max Ivashkevich
+ * @version 1.7
+ * @since 1.0
+ * Date Complete: 2020-04-19
  */
 public class DealCards extends GroupOfCards {
-      private int handSize = 5;
-      //Card[] cards = new Card[handSize];
-    
-    DealCards(int size){
-    super(size);
-    }
-        public void dealHand(){
-            int countCards = 0;
-            Random random = new Random();
-            
-            Card c = new Card();
-            int x =c.getValue().values().length;
-            int y =c.getSuit().values().length;
-            for(int i = 0; i < handSize; i++){
-                c = new Card(y,x);
-                c.getSuit();
-                c.getValue();
-                //cards[countCards] = (new Card());
-                countCards++;
-            }//end outter for
-        }//end method
 
+    private int handSize = 5;
+    /**
+    * @return the hand size
+    */
+    //Card[] cards = new Card[handSize];
+    
+    DealCards(int size) {
+        super(size);
     }
+
+    public void dealHand() {
+        int countCards = 0;
+        Random random = new Random();
+        Card c = new Card();
+        int x = c.getValue().values().length;
+        int y = c.getSuit().values().length;
+        /**
+         * declares a card counter to make a hand of 5-7 depending on the players
+         * Makes an instance of the Card class to use the getSuit/Value methods
+         */
+        for (int i = 0; i < handSize; i++) {
+            c = new Card(y, x);
+            c.getSuit();
+            c.getValue();
+            //cards[countCards] = (new Card());
+            countCards++;
+        }//end outter for
+    }//end method
+
+}
