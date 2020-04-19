19fb6dc2cc689458685926f15e1b9ccd5be04ada
Vic <Vic@DESKTOP-GO5GRHQ>
2020-04-19 19:49:56 -0400

    Commit: Comments added to the game class

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -3,84 +3,88 @@
  * Students can modify and extend to implement their game.
  * Add your name as a modifier and the date!
  */
-
 package ca.sheridancollege.project;
+
 import java.util.Scanner;
 
 import java.util.ArrayList;
 
 /**
- * The class that models your game. You should create a more specific
- * child of this class and instantiate the methods given.
- * @author dancye
+ * The class that models your game. You should create a more specific child of
+ * this class and instantiate the methods given.
+ *
+ * @modifier Ahmad Al Sayyed, Daniel Ayala, Max Ivashkevich
+ * @version 1.7
+ * @since 1.0
+ * Date Complete: 2020-04-19
  */
-public class Game 
-{
+public class Game {
+
     private final String gameName;//the title of the game
-    private ArrayList <Player> players = new ArrayList<Player>();// the players of the game // private
+    private ArrayList<Player> players = new ArrayList<Player>();// the players of the game // private
     private int numberOfPlayers;
-    
-    public Game(String givenName)
-    {
+    /**
+     * For getting the players and their names
+     * @param givenName gets the players names
+     * @param numberOfPlayers gets the amount of players
+     */
+
+    public Game(String givenName) {
         gameName = givenName;
         players = new ArrayList();
     }
-
     /**
-     * @return the gameName
+     * makes the game with the players name and makes an arraylist of players
+     * @return the gameName is the name of the player inside the game
      */
-    public String getGameName() 
-    {
+    
+    public String getGameName() {
         return gameName;
     }
-    
-     /**
+    /**
      * @return the players of this game
      */
-    public ArrayList <Player> getPlayers(String nameOfPlayer) 
-    {
+    
+    public ArrayList<Player> getPlayers(String nameOfPlayer) {
         //Player player = new Player(nameOfPlayer);
         //players.add();
         return players;
     }
-
     /**
      * @param players the players of this game
      */
-    public void setPlayers(ArrayList <Player> players) 
-    {
+    
+    public void setPlayers(ArrayList<Player> players) {
         this.players = players;
     }
-    
-    
     /**
-     * Play the game. This might be one method or many method calls depending
-     * on your game.
+     * @return sets the players into the arraylist
      */
-    public  void startGame(){
-        
+    
+    public void startGame() {
+
         Scanner input = new Scanner(System.in);
         System.out.println("You are playing " + gameName);
         System.out.println("Please enter number of players");
         numberOfPlayers = input.nextInt();
-        
-                for(int i=0;i < numberOfPlayers;i++){
-                    System.out.println("Please name of player " + (i + 1) + ": ");           
-                    Player player = new Player(input.next());
-                }
-            
-        
-        
-        System.out.println("Start Game");
-        
-    }
-    
-    /**
-     * When the game is over, use this method to declare and display a winning
-     * player.
-     */
-    public  void declareWinner(){}
 
-   
+        for (int i = 0; i < numberOfPlayers; i++) {
+            System.out.println("Please name of player " + (i + 1) + ": ");
+            Player player = new Player(input.next());
+        }
+
+        System.out.println("Start Game");
+
+    }
+    /**
+     * Starts the game
+     */
     
+    public void declareWinner() {
+    }
+    /**
+     * 
+     * @return the winner of the game
+     */
+
 }//end class
