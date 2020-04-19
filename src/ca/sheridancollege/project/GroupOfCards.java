019f366c59485fcfa1a6ba69e237d90e6a7e1806
Vic <Vic@DESKTOP-GO5GRHQ>
2020-04-19 19:58:03 -0400

    Commit: Comments added to the GroupOfCards class

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -9,36 +9,48 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- * A concrete class that represents any grouping of cards for a Game.
- * HINT, you might want to subclass this more than once.
- * The group of cards has a maximum size attribute which is flexible for reuse.
- * @author dancye
+ * A concrete class that represents any grouping of cards for a Game. HINT, you
+ * might want to subclass this more than once. The group of cards has a maximum
+ * size attribute which is flexible for reuse.
+ *
+ * @modifier Ahmad Al Sayyed, Daniel Ayala, Max Ivashkevich
+ * @version 1.7
+ * @since 1.0
+ * Date Complete: 2020-04-19
  */
-public class GroupOfCards 
-{
-   
-    //The group of cards, stored in an ArrayList
-    protected ArrayList <Card> cards = new ArrayList<Card>();
-    private int size;//the size of the grouping
-    
-    public GroupOfCards(int givenSize)
-    {
+public class GroupOfCards {
+
+    protected ArrayList<Card> cards = new ArrayList<Card>();
+    private int size;
+    /**
+     *  The group of cards, stored in an ArrayList
+     *  @param size the size of the grouping
+     */
+
+    public GroupOfCards(int givenSize) {
         size = givenSize;
     }
-    
     /**
      * A method that will get the group of cards as an ArrayList
+     * @param givenSize size of the hand
      * @return the group of cards.
      */
-    public ArrayList<Card> showCards(){
+    
+    public ArrayList<Card> showCards() {
         return cards;
     }
-    
-    public void shuffle()
-    {
+    /**
+     * @return the cards in the hand
+     */
+
+    public void shuffle() {
         Collections.shuffle(cards);
     }
-
+    /**
+     * @return shuffled cards
+     */
+    
+    
     /**
      * @return the size of the group of cards
      */
@@ -52,5 +64,5 @@ public class GroupOfCards
     public void setSize(int givenSize) {
         size = givenSize;
     }
-    
+
 }//end class
