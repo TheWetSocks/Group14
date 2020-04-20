cd051c69d926756adcb5093d6b0e5ce7987f42b1
Vic <Vic@DESKTOP-GO5GRHQ>
2020-04-19 20:29:01 -0400

    Commit: Comments added to the Player class

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -1,31 +1,45 @@
-
 package ca.sheridancollege.project;
 
-
-public class Player 
-{
+/**
+ * @modifier Ahmad Al Sayyed, Daniel Ayala, Max Ivashkevich
+ * @version 1.7
+ * @since 1.0
+ * Date Complete: 2020-04-19
+ */
+public class Player {
+    
     private String playerID; //the unique ID for this player
     private int handSize = 4;
     Card[] cards = new Card[handSize];
-   
-    public Player(String name)
-    {
-        playerID= name;
+    
+    /**
+     * A constructor for the player that has a string param for the players name
+     * @param name the name of the player 
+     */
+    public Player(String name) {
+        playerID = name;
     }
-
-  
-    public String getPlayerID() 
-    {
+    
+    /**
+     * Method for getting the id of the player
+     * @return the players id/name
+     */
+    public String getPlayerID() {
         return playerID;
     }
-
-   
-    public void setPlayerID(String givenID) 
-    {
+    
+    /**
+     * Sets the players name or id to the users choice
+     * @param givenID the id the player chooses
+     */
+    public void setPlayerID(String givenID) {
         playerID = givenID;
     }
-
-  
-    public void play(){}
+    
+    /**
+     * 
+     */
+    public void play() {
+    }
 
 }
